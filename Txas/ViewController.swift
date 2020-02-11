2652a646e44bff332debe4f26bb539bb14f181dd
HuijieChu <55219787+HuijieChu@users.noreply.github.com>
2020-02-10 19:14:52 -0500

    UIdrag_origin_version

--- Txas/ViewController.swift
+++ Txas/ViewController.swift
@@ -12,85 +12,128 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //view.bringSubviewToFront(a1)
         // Do any additional setup after loading the view.
     }
     
-    @IBOutlet weak var button_1: UIButton!
-    @IBOutlet weak var button_2: UIButton!
-    @IBOutlet weak var button_3: UIButton!
-    @IBOutlet weak var button_4: UIButton!
-    @IBOutlet weak var button_5: UIButton!
-    @IBOutlet weak var button_6: UIButton!
-    @IBOutlet weak var button_7: UIButton!
-    @IBOutlet weak var button_8: UIButton!
-    @IBOutlet weak var button_9: UIButton!
-    @IBOutlet weak var button_10: UIButton!
-    @IBOutlet weak var button_11: UIButton!
-    @IBOutlet weak var button_12: UIButton!
-    @IBOutlet weak var button_13: UIButton!
+    @IBOutlet weak var image_1: UIImageView!
+    @IBOutlet weak var image_2: UIImageView!
     
+    @IBOutlet weak var image_3: UIImageView!
+    
+    @IBOutlet weak var image_4: UIImageView!
+    
+    @IBOutlet weak var image_5: UIImageView!
+    
+    @IBOutlet weak var image_6: UIImageView!
+    @IBOutlet weak var image_7: UIImageView!
+    
+    @IBOutlet weak var image_8: UIImageView!
+    @IBOutlet weak var image_9: UIImageView!
+    
+    @IBOutlet weak var image_10: UIImageView!
+    
+    @IBOutlet weak var image_11: UIImageView!
+    
+    @IBOutlet weak var image_12: UIImageView!
+    
+    @IBOutlet weak var image_13: UIImageView!
+    
+    @IBOutlet weak var a1: UIImageView!
+    @IBOutlet weak var a2: UIImageView!
+    @IBOutlet weak var a3: UIImageView!
+    @IBOutlet weak var a4: UIImageView!
+    @IBOutlet weak var a5: UIImageView!
+    @IBOutlet weak var hand1: UIImageView!
+    
+    @IBOutlet weak var hand2: UIImageView!
     @IBAction func spadeButton(_ sender: UIButton) {
-        button_1.setImage(#imageLiteral(resourceName: "a1"), for:.normal);
-        button_2.setImage(#imageLiteral(resourceName: "a2") , for:.normal);
-        button_3.setImage(#imageLiteral(resourceName: "a3") , for:.normal);
-        button_4.setImage(#imageLiteral(resourceName: "a4") , for:.normal);
-        button_5.setImage(#imageLiteral(resourceName: "a5") , for:.normal);
-        button_6.setImage(#imageLiteral(resourceName: "a6") , for:.normal);
-        button_7.setImage(#imageLiteral(resourceName: "a7") , for:.normal);
-        button_8.setImage(#imageLiteral(resourceName: "a8") , for:.normal);
-        button_9.setImage(#imageLiteral(resourceName: "a9") , for:.normal);
-        button_10.setImage(#imageLiteral(resourceName: "a10") , for:.normal);
-        button_11.setImage(#imageLiteral(resourceName: "a11") ,for:.normal);
-        button_12.setImage(#imageLiteral(resourceName: "a12") , for:.normal);
-        button_13.setImage(#imageLiteral(resourceName: "a13") , for:.normal);
+        image_1.image = #imageLiteral(resourceName: "a1");
+        image_2.image = #imageLiteral(resourceName: "a2") ;
+        image_3.image = #imageLiteral(resourceName: "a3") ;
+        image_4.image = #imageLiteral(resourceName: "a4") ;
+        image_5.image = #imageLiteral(resourceName: "a5") ;
+        image_6.image = #imageLiteral(resourceName: "a6") ;
+        image_7.image = #imageLiteral(resourceName: "a7") ;
+        image_8.image = #imageLiteral(resourceName: "a8") ;
+        image_9.image = #imageLiteral(resourceName: "a9") ;
+        image_10.image = #imageLiteral(resourceName: "a10") ;
+        image_11.image = #imageLiteral(resourceName: "a11") ;
+        image_12.image = #imageLiteral(resourceName: "a12") ;
+        image_13.image = #imageLiteral(resourceName: "a13") ;
         
     }
     @IBAction func heartsButton(_ sender: UIButton) {
-        button_1.setImage(#imageLiteral(resourceName: "b1") , for:.normal);
-        button_2.setImage(#imageLiteral(resourceName: "b2") , for:.normal);
-        button_3.setImage(#imageLiteral(resourceName: "b3") , for:.normal);
-        button_4.setImage(#imageLiteral(resourceName: "b4") , for:.normal);
-        button_5.setImage(#imageLiteral(resourceName: "b5") , for:.normal);
-        button_6.setImage(#imageLiteral(resourceName: "b6") , for:.normal);
-        button_7.setImage(#imageLiteral(resourceName: "b7") , for:.normal);
-        button_8.setImage(#imageLiteral(resourceName: "b8") , for:.normal);
-        button_9.setImage(#imageLiteral(resourceName: "b9") , for:.normal);
-        button_10.setImage(#imageLiteral(resourceName: "b10") , for:.normal);
-        button_11.setImage(#imageLiteral(resourceName: "b11") ,for:.normal);
-        button_12.setImage(#imageLiteral(resourceName: "b12") , for:.normal);
-        button_13.setImage(#imageLiteral(resourceName: "b13") , for:.normal);
+        image_1.image = #imageLiteral(resourceName: "b1") ;
+        image_2.image = #imageLiteral(resourceName: "b2") ;
+        image_3.image = #imageLiteral(resourceName: "b3") ;
+        image_4.image = #imageLiteral(resourceName: "b4") ;
+        image_5.image = #imageLiteral(resourceName: "b5") ;
+        image_6.image = #imageLiteral(resourceName: "b6") ;
+        image_7.image = #imageLiteral(resourceName: "b7") ;
+        image_8.image = #imageLiteral(resourceName: "b8") ;
+        image_9.image = #imageLiteral(resourceName: "b9") ;
+        image_10.image = #imageLiteral(resourceName: "b10") ;
+        image_11.image = #imageLiteral(resourceName: "b11") ;
+        image_12.image = #imageLiteral(resourceName: "b12") ;
+        image_13.image = #imageLiteral(resourceName: "b13") ;
     }
     @IBAction func plumButton(_ sender: UIButton) {
-        button_1.setImage(#imageLiteral(resourceName: "c1") , for:.normal);
-        button_2.setImage(#imageLiteral(resourceName: "c2") , for:.normal);
-        button_3.setImage(#imageLiteral(resourceName: "c3") , for:.normal);
-        button_4.setImage(#imageLiteral(resourceName: "c4") , for:.normal);
-        button_5.setImage(#imageLiteral(resourceName: "c5") , for:.normal);
-        button_6.setImage(#imageLiteral(resourceName: "c6") , for:.normal);
-        button_7.setImage(#imageLiteral(resourceName: "c7") , for:.normal);
-        button_8.setImage(#imageLiteral(resourceName: "c8") , for:.normal);
-        button_9.setImage(#imageLiteral(resourceName: "c9") , for:.normal);
-        button_10.setImage(#imageLiteral(resourceName: "c10") , for:.normal);
-        button_11.setImage(#imageLiteral(resourceName: "c11") ,for:.normal);
-        button_12.setImage(#imageLiteral(resourceName: "c12") , for:.normal);
-        button_13.setImage(#imageLiteral(resourceName: "c13") , for:.normal);
+        image_1.image = #imageLiteral(resourceName: "c1") ;
+        image_2.image = #imageLiteral(resourceName: "c2") ;
+        image_3.image = #imageLiteral(resourceName: "c3") ;
+        image_4.image = #imageLiteral(resourceName: "c4") ;
+        image_5.image = #imageLiteral(resourceName: "c5") ;
+        image_6.image = #imageLiteral(resourceName: "c6") ;
+        image_7.image = #imageLiteral(resourceName: "c7") ;
+        image_8.image = #imageLiteral(resourceName: "c8") ;
+        image_9.image = #imageLiteral(resourceName: "c9") ;
+        image_10.image = #imageLiteral(resourceName: "c10") ;
+        image_11.image = #imageLiteral(resourceName: "c11") ;
+        image_12.image = #imageLiteral(resourceName: "c12") ;
+        image_13.image = #imageLiteral(resourceName: "c13") ;
     }
     @IBAction func squareButton(_ sender: UIButton) {
-        button_1.setImage(#imageLiteral(resourceName: "d1") , for:.normal);
-        button_2.setImage(#imageLiteral(resourceName: "d2") , for:.normal);
-        button_3.setImage(#imageLiteral(resourceName: "d3") , for:.normal);
-        button_4.setImage(#imageLiteral(resourceName: "d4") , for:.normal);
-        button_5.setImage(#imageLiteral(resourceName: "d5") , for:.normal);
-        button_6.setImage(#imageLiteral(resourceName: "d6") , for:.normal);
-        button_7.setImage(#imageLiteral(resourceName: "d7") , for:.normal);
-        button_8.setImage(#imageLiteral(resourceName: "d8") , for:.normal);
-        button_9.setImage(#imageLiteral(resourceName: "d9") , for:.normal);
-        button_10.setImage(#imageLiteral(resourceName: "d10") , for:.normal);
-        button_11.setImage(#imageLiteral(resourceName: "d11") ,for:.normal);
-        button_12.setImage(#imageLiteral(resourceName: "d12") , for:.normal);
-        button_13.setImage(#imageLiteral(resourceName: "d13") , for:.normal);
+        image_1.image = #imageLiteral(resourceName: "d1") ;
+        image_2.image = #imageLiteral(resourceName: "d2") ;
+        image_3.image = #imageLiteral(resourceName: "d3") ;
+        image_4.image = #imageLiteral(resourceName: "d4") ;
+        image_5.image = #imageLiteral(resourceName: "d5") ;
+        image_6.image = #imageLiteral(resourceName: "d6") ;
+        image_7.image = #imageLiteral(resourceName: "d7") ;
+        image_8.image = #imageLiteral(resourceName: "d8") ;
+        image_9.image = #imageLiteral(resourceName: "d9") ;
+        image_10.image = #imageLiteral(resourceName: "d10") ;
+        image_11.image = #imageLiteral(resourceName: "d11") ;
+        image_12.image = #imageLiteral(resourceName: "d12") ;
+        image_13.image = #imageLiteral(resourceName: "d13") ;
     }
     
+    var fileViewOrigin: CGPoint!
+    @IBAction func pan_button1(_ sender: UIPanGestureRecognizer) {
+        guard sender.view! != nil else{return}
+        let piece = sender.view!
+        view.bringSubviewToFront(image_1)
+        let translation = sender.translation(in:view)
+        //view.bringSubviewToFront(a1)
+        fileViewOrigin = a1.frame.origin
+        if sender.state == .began || sender.state == .changed
+        {
+            piece.center = CGPoint(x:piece.center.x + translation.x,y:piece.center.y + translation.y)
+            sender.setTranslation(CGPoint.zero, in:view)
+        }
+        if sender.state == .ended
+        {
+            if piece.frame.intersects(image_1.frame)
+            {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.a1.alpha = 0.0
+                })
+            }
+        }
+        
+    }
+
     
 }
 
